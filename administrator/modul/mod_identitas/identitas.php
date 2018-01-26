0cb1c468ffa91dc99845a49dd37de3b44c5402e5
herry88 <herry081288@gmail.com>
2018-01-26 23:04:18 +0700

    Modul Identitas OK

--- administrator/modul/mod_identitas/identitas.php
+++ administrator/modul/mod_identitas/identitas.php
@@ -30,11 +30,22 @@
   // Tampil identitas
   default:
     $record  = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM identitas LIMIT 1"));
-    echo "<div class='col-md-12'>
+    echo "
+    <!-- Content Header (Page header) -->
+    <section class=\"content-header\">
+      <h1>
+        Data Identitas Web</h1>
+      <ol class=\"breadcrumb\">
+        <li><a href=\"?module=beranda\"><i class=\"fa fa-dashboard\"></i> Home</a></li>
+        <li class=\"active\">Data Identitas</li>
+      </ol>
+    </section>
+    <section class='content'>
+    <div class='col-md-12'>
               <div class='box box-info'>
                 <div class='box-header with-border'>
                   <h3 class='box-title'>Identitas Website</h3>
-                </div>
+            </div>
               <div class='box-body'>
               <form class='form-horizontal' role='form' method=POST enctype='multipart/form-data' action=$aksi?module=identitas&act=update>
               <input type=hidden name=id value=$record[id_identitas]>
@@ -64,7 +75,8 @@
                     
                   </div>
             </div>
-        </div>";
+        </div>
+        </section>";
 
     break;  
         }
